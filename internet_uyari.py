531b43cde6e18ad9b315ca211bad7ade1b733807
Yiğit Çıtak <yigitcitak007@gmail.com>
2025-03-23 18:29:23 +0300

    Add files via upload

---
+++ internet_uyari.py
@@ -0,0 +1,5 @@
+import customtkinter as ctk
+
+def wid(window):
+	Mesaj = ctk.CTkLabel(window,text="İnternet bağlantınızı kontrol edin :/",font=("italic",35),text_color="white")
+	Mesaj.pack(pady=60)
